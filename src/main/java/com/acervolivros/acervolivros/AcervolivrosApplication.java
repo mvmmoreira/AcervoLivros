9ba10723cc405093f490a8de794ff52cfef4d7ee
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-07-08 20:16:56 -0300

    chore: adiciona estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/acervolivros/acervolivros/AcervolivrosApplication.java
@@ -0,0 +1,13 @@
+package com.acervolivros.acervolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervolivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervolivrosApplication.class, args);
+	}
+
+}
